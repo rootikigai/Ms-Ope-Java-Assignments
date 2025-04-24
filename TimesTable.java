c5f30f2cdd5e549502942e8ffc6eb71e271d88eb
rootikigai <ifykel360@gmail.com>
2025-04-24 23:55:05 +0100

    Learning Java Iteration

---
+++ TimesTable.java
@@ -0,0 +1,12 @@
+public class TimesTable{
+	public static void main(String[] args){
+
+    System.out.println("2 Times Table");
+    for(int r = 1; r <= 12; r++){
+      for(int c = 1; c <= 3; c++){
+        System.out.print(r * c + "\t");
+      }
+    System.out.println();
+    }
+	}
+}
